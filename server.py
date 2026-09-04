dfb7b6da23b84e7a3fa964307d3e0bd14ca40470
Feon1 <slider81@yandex.ru>
2026-09-04 09:12:50 +0300

    Update server.py

--- server.py
+++ server.py
@@ -344,7 +344,7 @@ async def telegram_webhook(update: dict):
             return {"ok": True}
         try:
             # 👇 ВСТАВЬТЕ СЮДА ВАШЕ СООБЩЕНИЕ
-            await send_telegram_message(chat_id, "Актуальная версия бота: https://feon-chat.website.yandexcloud.net/")
+            await send_telegram_message(chat_id, "Актуальная версия бота: https://max.ru/se13654625_bot")
 
             response_text = await process_message_core(user_id, text)
             await send_telegram_message(chat_id, response_text)
